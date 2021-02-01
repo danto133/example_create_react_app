cac277ffca2302bb5c6f45d94130d8244b1c4c8f
Dan To Hoa <danto13397@gmail.com>
2021-02-02 00:21:23 +0700

    :hammer: Change js to ts

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.scss";
+
+const App: React.FC = () => <div className="example">React App</div>;
+
+const AppWrapper: React.FC = () => (
+  // TODO: Update Provider later
+  <App />
+);
+
+export default AppWrapper;
