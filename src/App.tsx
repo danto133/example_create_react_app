1d0b1e0863275b392172b120787e17136006480e
Dan To Hoa <dan.to@3forcom.com.vn>
2021-02-04 11:59:11 +0700

    :sparkles: Wrapper Provider into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import routes from "routes";
+import { getAccessToken } from "services/common/storage";
+import store from "store";
 import "./App.scss";
 
-const App: React.FC = () => (
-  <div className="App">
-    App
-  </div>
-);
+const NotFound: React.FC = () => <div>Page Not Found</div>;
+
+/* TODO: Implement Page SignIn later */
+const SignIn: React.FC = () => <div>Sign In</div>;
+
+const App: React.FC = () => {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+      setToken(accessToken);
+    }
+  }, []);
+
+  return (
+    <Router>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route
+            key={index.toString()}
+            exact
+            path={route.path}
+            component={
+              // eslint-disable-next-line no-nested-ternary
+              route.isAuth
+                ? token
+                  ? route.component
+                  : SignIn
+                : route.component
+            }
+          />
+        ))}
+        <Route exact path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
 
 const AppWrapper: React.FC = () => (
-  // TODO: Update Provider later
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 export default AppWrapper;
